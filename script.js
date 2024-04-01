0f291e9fb587706f2728d265a534712bd56c243f
Deo Seth <diothewind@gmail.com>
2024-04-01 23:36:16 +0800

    Add function to display books through DOM manipulation

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-const library = [];
+const cardContainer = document.querySelector("#card-container")
+
+const library = [
+    {title: "The Silent Patient", author: "Deo Seth Jabagat", pageCount: 45, readStatus: false}, 
+    {title: "What the Hell", author: "Mark Pratesh", pageCount: 512, readStatus: true}];
 
 function Book(title, author, pageCount, readStatus) {
     this.title = title;
@@ -6,3 +10,75 @@ function Book(title, author, pageCount, readStatus) {
     this.pageCount = pageCount;
     this.readStatus = readStatus;
 }
+
+function displayLibrary(library) {
+    library.forEach((book) => {
+        const card = createCard(book);
+        cardContainer.appendChild(card);
+    });
+}
+
+function createCard(book) {
+    const card = document.createElement("div");
+    const cardBody = document.createElement("div");
+    const bookTitle = document.createElement("h2");
+    const bookAuthor = document.createElement("p");
+    const bookNumberOfPages = document.createElement("p");
+    const bookStatus = document.createElement("p");
+    const btnContainer = document.createElement("div");
+    const changeStatusBtn = document.createElement("button");
+    const deleteBookBtn = document.createElement("button");
+
+    bookTitle.textContent = book.title;
+    // bookTitle.classList.add("card-title")
+    bookTitle.setAttribute("class", "card-title");
+
+    bookAuthor.textContent = "Author: " + book.author;
+    // bookAuthor.classList.add("text-gray-600");
+    bookAuthor.setAttribute("class", "text-gray-600");
+
+    bookNumberOfPages.textContent = "Number of pages: " + book.pageCount;
+    // bookNumberOfPages.classList.add("text-gray-600");
+    bookNumberOfPages.setAttribute("class", "text-gray-600");
+
+    if(book.readStatus) {
+        bookStatus.textContent = "Already read";
+        // bookStatus.classList.add("text-success");
+        bookStatus.setAttribute("class", "text-success");
+    } else {
+        bookStatus.textContent = "Haven't read yet";
+        // bookStatus.classList.add("text-error");
+        bookStatus.setAttribute("class", "text-error");
+    }
+
+    changeStatusBtn.textContent = "Change Read Status";
+    // changeStatusBtn.classList.add("btn btn-info");
+    changeStatusBtn.setAttribute("class", "btn btn-info");
+
+    deleteBookBtn.textContent = "Remove Book";
+    // deleteBookBtn.classList.add("btn btn-error");
+    deleteBookBtn.setAttribute("class", "btn btn-error");
+
+    btnContainer.appendChild(changeStatusBtn);
+    btnContainer.appendChild(deleteBookBtn);
+    // btnContainer.classList.add("card-actions justify-end mt-5");
+    btnContainer.setAttribute("class", "card-actions justify-end mt-5");
+
+    cardBody.appendChild(bookTitle);
+    cardBody.appendChild(bookAuthor);
+    cardBody.appendChild(bookNumberOfPages);
+    cardBody.appendChild(bookStatus);
+    cardBody.appendChild(btnContainer);
+    // cardBody.classList.add("card-body");
+    cardBody.setAttribute("class", "card-body");
+
+    card.appendChild(cardBody);
+    // card.classList.add("card w-96 bg-base-200 shadow-xl");
+    card.setAttribute("class", "card w-96 bg-base-200 shadow-xl");
+
+    return card;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    displayLibrary(library);
+});
