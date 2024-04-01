c45914f11d540dc6125b1a5c885967bfc1438a1b
Deo Seth <diothewind@gmail.com>
2024-04-01 22:41:14 +0800

    Setup JS

---
+++ script.js
@@ -0,0 +1,8 @@
+const library = [];
+
+function Book(title, author, pageCount, readStatus) {
+    this.title = title;
+    this.author = author;
+    this.pageCount = pageCount;
+    this.readStatus = readStatus;
+}
