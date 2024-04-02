e02bdcfe3a7592ab95442371854f1abde0ac0b5b
Deo Seth <diothewind@gmail.com>
2024-04-02 16:56:29 +0800

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./css/**/*.{html,js}", "./index.html"],
+  content: ["./css/**/*.{html,js}", "./index.html", "./script.js"],
   theme: {
     extend: {
       fontFamily: {
-        'poppins': ["Poppins"],
-      }
+        poppins: ["Poppins"],
+      },
     },
   },
   plugins: [require("daisyui")],
 
   daisyui: {
     themes: ["retro"],
-  }
-}
-
+  },
+};
